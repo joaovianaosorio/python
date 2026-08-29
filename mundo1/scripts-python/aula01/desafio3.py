4edf9f4b3558ce0f8d1dbb4836449ec9924050ca
João Vitor Osório <joaovianaosorio@gmail.com>
2026-08-29 20:05:09 -0300

    primeiros códigos

---
+++ mundo1/scripts-python/aula01/desafio3.py
@@ -0,0 +1,5 @@
+# Código vai dar errado. Aprenderei como fazer certo nas próximas aulas.
+print('Digite dois números para vermos a soma deles')
+numero1 = input('Primeiro número:')
+numero2 = input('Segundo número:')
+print('A soma é', numero1 + numero2)
